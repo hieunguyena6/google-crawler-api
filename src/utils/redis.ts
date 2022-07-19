70574b3720c0424613e1db198bea226b3326d1bb
Manh Hieu From Telio <manhhieu@MAC-ManhHieu.local>
2022-07-19 23:00:47 +0700

    feat: add redis cache

---
+++ src/utils/redis.ts
@@ -0,0 +1,7 @@
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+});
+
+export default redisClient;
